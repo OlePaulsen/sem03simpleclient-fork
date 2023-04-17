31ab36803fcb88a393e0e233fae68e78cb069266
root <olepaulsen@outlook.com>
2023-04-17 16:37:53 +0000

    1

--- tcp_client.go
+++ tcp_client.go
@@ -6,15 +6,38 @@ import (
 	"os"
 )
 
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:; ")
+
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+	kryptertMelding := make([]rune, len(melding))
+	for i := 0; i < len(melding); i++ {
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		newIndex := (indeks + chiffer) % len(alphabet)
+		kryptertMelding[i] = alphabet[newIndex]
+	}
+	return kryptertMelding
+}
+
+func sokIAlfabetet(symbol rune, alfabet []rune) int {
+	for i := 0; i < len(alfabet); i++ {
+		if symbol == alfabet[i] {
+			return i
+			break
+		}
+	}
+	return -1
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.2:44167")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
-
- 	_, err = conn.Write([]byte(os.Args[1]))
+kryptertMelding := Krypter([]rune(os.Args[1]), ALF_SEM03, 4)
+log.Println("Kryptert melding: ", string(kryptertMelding))
+_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,6 +46,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+dekryptertMelding := string(Krypter([]rune(string(buf[:n])), ALF_SEM03, len(ALF_SEM03)-4))
+
+	log.Printf("reply from proxy: %s", dekryptertMelding)
 }
+
